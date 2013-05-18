0e7721f8d3bbaa230c13380109172e86a9ee1bba
AlSayed Gamal <mail.gamal@gmail.com>
2013-05-18 13:46:37 +0200

    added some validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,15 @@
 class Book < ActiveRecord::Base
   attr_accessible :isbn, :title
   letsrate_rateable "rating"
+
+  validate :check_length
+  validates :isbn, :numericality => { :only_integer => true }
+  validates :isbn, :uniqueness => true
+  validates :title, :presence => true
+
+  def check_length
+    unless isbn.size == 10 or isbn.size == 13
+      errors.add(:isbn, "length must be 10 or 13") 
+    end
+  end
 end
